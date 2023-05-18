d83fc3e1aa9b1c211d4fb3c56f3b0edd2d8f523f
kingssjr <94551301+kingssjr@users.noreply.github.com>
2023-05-18 09:56:54 -0300

    alcides

---
+++ src/components/CarteiraDigital/LoginWallet.vue
@@ -0,0 +1,205 @@
+<template>
+  <v-card
+  color="black">
+    <v-layout>
+      <v-main style="min-height: 980px;">
+        <v-container>
+        <v-sheet class="mx-auto" density="comfortable" color="transparent" max-width="580">
+          
+          <!-- logo Howhow --> 
+          <v-img
+            class="mx-auto"
+            height="200"
+            max-width="400"
+            :width=" 240 "
+            aspect-ratio="16/9"
+            :src="require('@/assets/images/carteira/walletlogo.svg')"
+          />
+          <v-card-text>
+            <v-card color="black">
+              <v-tabs
+                v-model="tab"
+                bg-color="transparent"
+                align-tabs="center"
+              >
+                <v-tab value="Entrar" color="blue">Entrar</v-tab>
+                <v-tab value="Registrar" color="blue">Registrar Carteira</v-tab>
+              </v-tabs>
+
+              <v-card-text>
+                <v-window v-model="tab">
+                  <v-window-item value="Entrar">
+                    <v-card
+                      class="mx-auto"
+                      max-width="500"
+                      color="black"
+                    >
+                      <v-card-title class="text-h6 font-weight-regular justify-space-between">
+                        <span>{{ login }}</span>
+                        
+                      </v-card-title>
+
+                      <v-window v-model="step">
+                        <v-window-item :value="1">
+                          <v-card-text>
+                            <v-text-field
+                              label="Email"
+                              placeholder="john@google.com"
+                            ></v-text-field>
+                            <span class="text-caption text-grey-darken-1">
+                              Este é o e-mail que você registrou para acessar sua carteira HowHow
+                            </span>
+                          </v-card-text>
+                        </v-window-item>
+
+                        <v-window-item :value="2">
+                          <v-card-text>
+                            <v-text-field
+                              label="Senha"
+                              type="password"
+                            ></v-text-field>
+                            <span class="text-caption text-grey-darken-1">
+                              Por favor, insira sua senha para acessar a carteira
+                            </span>
+                          </v-card-text>
+                        </v-window-item>
+
+                        
+                      </v-window>
+
+                      <v-divider></v-divider>
+
+                      <v-card-actions>
+                        <v-btn
+                          v-if="step > 1"
+                          variant="text"
+                          @click="step--"
+                        >
+                          Voltar
+                        </v-btn>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                          v-if="step < 2"
+                          color="blue"
+                          variant="flat"
+                          @click="step++"
+                        >
+                          Avançar
+                        </v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-window-item>
+
+                  <v-window-item value="Registrar">
+                    <v-card
+                      class="mx-auto"
+                      max-width="500"
+                      color="black"
+                    >
+                      <v-card-title class="text-h6 font-weight-regular justify-space-between">
+                        <span>{{ registro }}</span>
+                      </v-card-title>
+
+                      <v-window v-model="step">
+                        <v-window-item :value="1">
+                          <v-card-text>
+                            <v-text-field
+                              label="Email"
+                              placeholder="john@google.com"
+                            ></v-text-field>
+                            <span class="text-caption text-grey-darken-1">
+                              Este é o e-mail que você usará para acessar sua Carteira HowHow
+                            </span>
+                          </v-card-text>
+                        </v-window-item>
+
+                        <v-window-item :value="2">
+                          <v-card-text>
+                            <v-text-field
+                              label="Senha"
+                              type="password"
+                            ></v-text-field>
+                            <v-text-field
+                              label="Confirme sua senha"
+                              type="password"
+                            ></v-text-field>
+                            <span class="text-caption text-grey-darken-1">
+                              Por favor, Crie uma senha para sua conta
+                            </span>
+                          </v-card-text>
+                        </v-window-item>
+
+                        <v-window-item :value="3">
+                          <div class="pa-4 text-center">
+                            <v-img
+                              class="mb-4"
+                              contain
+                              height="128"
+                              :src="require('@/assets/images/carteira/wallethowhow.svg')"
+                            ></v-img>
+                            <h3 class="text-h6 font-weight-light mb-2">
+                              Bem-vindo à Carteira Digital HowHow
+                            </h3>
+                            <span class="text-caption text-grey">Obrigado por inscrever-se!</span>
+                          </div>
+                        </v-window-item>
+                      </v-window>
+
+                      <v-divider></v-divider>
+
+                      <v-card-actions>
+                        <v-btn
+                          v-if="step > 1"
+                          variant="text"
+                          @click="step--"
+                        >
+                          Voltar
+                        </v-btn>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                          v-if="step < 3"
+                          color="blue"
+                          variant="flat"
+                          @click="step++"
+                        >
+                          Avançar
+                        </v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-window-item>
+
+                </v-window>
+              </v-card-text>
+            </v-card>
+          </v-card-text>
+          </v-sheet>
+        </v-container>
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
+<script>
+  export default {
+    data: () => ({
+      tab: null,
+      step: 1,
+    }),
+
+    computed: {
+      login() {
+        switch (this.step) {
+          case 1: return 'Digite seu email para entrar'
+          case 2: return 'Digite sua senha'
+          default: return ''
+        }
+      },
+      registro () {
+        switch (this.step) {
+          case 1: return 'Digite seu email para registro '
+          case 2: return 'Criar senha'
+          default: return 'Conta criada com sucesso'
+        }
+      },
+    },
+  }
+</script>
---
+++ src/components/CarteiraDigital/ProfileUserWallet.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-layout>
+      <v-main style="min-height: 980px;">
+        <v-container>
+        <v-sheet class="mx-auto" density="comfortable" color="transparent" max-width="580">
+          <v-card color="#242426" class="rounded-lg mx-2 pa-1 mt-n3 gradient ">
+            <v-toolbar flat color="transparent">
+            </v-toolbar>
+            <v-row justify="space-around">
+              <v-avatar size="300">
+                <v-img src="https://cdn.vuetifyjs.com/images/john.png" alt="John"></v-img>
+              </v-avatar>
+            </v-row>
+            <br><br><br>
+          </v-card> 
+         
+          <v-card-text>
+            <v-card color="white">
+              <v-tabs
+                v-model="tab"
+                bg-color="transparent"
+                align-tabs="center"
+              >
+                <v-tab value="test1" color="blue" prepend-icon="mdi-wallet-outline" stacked>Dados</v-tab>
+                <v-tab value="test2" color="blue" prepend-icon="mdi-bank-transfer-in" stacked>test</v-tab>
+                <v-tab value="test3" color="blue" prepend-icon="mdi-bank-transfer-out" stacked>test</v-tab>
+                <v-tab value="test4" color="blue" prepend-icon="mdi-bank-transfer" stacked>test</v-tab>
+
+              </v-tabs>
+
+              <v-card-text>
+                <v-window v-model="tab">
+                  <v-window-item value="test1">
+                    test1
+                  </v-window-item>
+
+                  <v-window-item value="test2">
+                    test2
+                  </v-window-item>
+
+                  <v-window-item value="test3">
+                    test3
+                  </v-window-item>
+
+                  <v-window-item value="test4">
+                    test4
+                  </v-window-item>
+
+                </v-window>
+              </v-card-text>
+            </v-card>
+          </v-card-text>
+          </v-sheet>
+        </v-container>
+      </v-main>
+    </v-layout>
+</template>
+<script>
+//Import Components
+
+
+  export default {
+    data: () => ({
+      tab: null,
+    }),
+    
+  }
+</script>
